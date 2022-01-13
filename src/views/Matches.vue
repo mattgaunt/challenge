329d3b52a3a263a9ba1ba721934eed5d618d607a
Mark Harwood <mark@phunky.co.uk>
2022-01-13 12:48:17 +0000

    Add crude get request for matches with basic loader

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -1,19 +1,39 @@
 <template>
 	<div class="p-8">
-    <div class="flex mt-11 gap-2">
+    <div class="flex mt-11 mb-8 gap-2">
       <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" aria-labelledby="calendar" role="presentation" aria-label="Upcoming" class="text-carbon-200"><g fill="currentColor"><path d="M3.71875 9.5H20.2812C20.5391 9.5 20.75 9.71094 20.75 9.96875V20.125C20.75 21.1602 19.9102 22 18.875 22H5.125C4.08984 22 3.25 21.1602 3.25 20.125V9.96875C3.25 9.71094 3.46094 9.5 3.71875 9.5ZM20.75 7.78125V6.375C20.75 5.33984 19.9102 4.5 18.875 4.5H17V2.46875C17 2.21094 16.7891 2 16.5312 2H14.9688C14.7109 2 14.5 2.21094 14.5 2.46875V4.5H9.5V2.46875C9.5 2.21094 9.28906 2 9.03125 2H7.46875C7.21094 2 7 2.21094 7 2.46875V4.5H5.125C4.08984 4.5 3.25 5.33984 3.25 6.375V7.78125C3.25 8.03906 3.46094 8.25 3.71875 8.25H20.2812C20.5391 8.25 20.75 8.03906 20.75 7.78125Z"></path></g></svg>
       <h1 class="text-3xl text-white leading-7 font-semibold">
         Upcoming
       </h1>
+    </div>
+    <div class="p-4 bg-carbon-500 rounded-lg">
+      <h1 v-if="loading">Loading...</h1>
+      <div v-else v-for="(match, index) in matches" :key="index">
+        <pre class="text-xs">{{ match }}</pre>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
 
 export default Vue.extend({
-  components: {
+  data () {
+    return {
+      matches: [],
+      loading: true,
+    }
+  },
+  beforeMount () {
+    axios.get('https://api.midnite.com/v0/matches/upcoming')
+      .then(response => {
+        this.matches = response.data
+      })
+      .finally(() => {
+        this.loading = false;
+      })
   }
 })
 </script>
