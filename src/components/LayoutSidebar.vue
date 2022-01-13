8d2f7632da5ac0ce68b15bf8ec8b8be46d376d6f
Mark Harwood <mark@phunky.co.uk>
2022-01-13 12:11:21 +0000

    Style the sidebar items

--- src/components/LayoutSidebar.vue
+++ src/components/LayoutSidebar.vue
@@ -1,12 +1,18 @@
 <template>
-  <nav class="flex w-60 bg-carbon-800">
-    <ol>
+  <nav class="bg-carbon-800">
+    <ol class="w-60 px-2 py-3">
       <li
         v-for="(item, index) in items"
         :key="index"
-        :class="item.type"
+        class="flex items-center h-12 p-3 text-white"
+        :class="[item.class, item.type]"
       >
-        {{ item.label }}
+        <div class="flex-grow">{{ item.label }}</div>
+        <div
+          v-if="item.count"
+          v-text="item.count"
+          class="rounded-lg py-1 px-3 font-medium text-xs text-green-400 bg-carbon-600 "
+        />
       </li>
     </ol>
   </nav>
@@ -23,6 +29,7 @@ export default Vue.extend({
         {
           type: 'link',
           label: 'Upcoming',
+          class: 'active'
         },
         {
           type: 'link',
@@ -30,8 +37,9 @@ export default Vue.extend({
           count: 2,
         },
         {
-          type: 'promoted',
+          type: 'link',
           label: 'Odds Boost',
+          class: 'promoted',
         },
         {
           type: 'heading',
@@ -50,3 +58,21 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style lang="scss" scoped>
+.heading {
+  margin-top: 12px;
+  color: var(--color-carbon-200);
+}
+
+.active {
+  background-color: var(--color-carbon-500);
+  border-radius: 12px;
+}
+
+.promoted {
+  background-image: linear-gradient(0, #FECA59, #FF7D63);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
